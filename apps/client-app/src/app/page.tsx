063af8abce72995efda9a29f4827c55b21ad2fbd
harunath <harunath04@gmail.com>
2025-08-23 13:27:10 +0530

    first commit

---
+++ apps/client-app/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+	return (
+		<div className="min-h-screen w-screen bg-white">
+			<main className="text-6xl text-green-300">Client App</main>
+		</div>
+	);
+}
---
+++ apps/merchant-app/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+	return (
+		<div className="min-h-screen w-screen bg-white">
+			<main className="text-6xl text-green-300">Merchant App</main>
+		</div>
+	);
+}
